bdc94264eb64a9a345af8fe2caafa4c3037c764e
TheWolfStudios <thewolfstudieos@gmail.com>
2026-04-28 22:08:02 -0500

    Added in new variable for Units

--- Data/UnitData.gd
+++ Data/UnitData.gd
@@ -3,3 +3,9 @@ extends Object
 
 var name: String = "None"
 var speed: float = 200.0
+var faction: String = "Neutral"
+# Player - owned and controlled by the player, attacks what ever they have their units set to attack.
+# Neutral - passive unowned mobs that don't automatically attack unless triggered to elsewhere
+# Hostile - naturally aggressive unowned mobs.
+# Other faction names can be specified later for specific behaviors, but the default function should be based on neutral.
+
